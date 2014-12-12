913743a33c18ccf6cb2b9059d65f21c454819a5c
Kleber <sistemas.kleber@bellinatiperez.com.br>
2014-12-12 11:49:01 -0200

    retirando as referencias

--- autoload.php
+++ autoload.php
@@ -1,26 +1,4 @@
 <?php
-/**
- * Copyright 2014 Facebook, Inc.
- *
- * You are hereby granted a non-exclusive, worldwide, royalty-free license to
- * use, copy, modify, and distribute this software in source code or binary
- * form for use in connection with the web services and APIs provided by
- * Facebook.
- *
- * As with any software that integrates with the Facebook platform, your use
- * of this software is subject to the Facebook Developer Principles and
- * Policies [http://developers.facebook.com/policy/]. This copyright notice
- * shall be included in all copies or substantial portions of the software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- */
 
 /**
  * You only need this file if you are not using composer.
@@ -28,12 +6,12 @@
  * https://getcomposer.org/
  */
 
-if (version_compare(PHP_VERSION, '5.4.0', '<')) {
-  throw new Exception('The Facebook SDK v4 requires PHP version 5.4 or higher.');
+if (version_compare(PHP_VERSION, '5.3.3', '<')) {
+  throw new Exception('The tecnospeed SDK v1 requires PHP version 5.3.3 or higher.');
 }
 
 /**
- * Register the autoloader for the Facebook SDK classes.
+ * Register the autoloader for the tecnospeed SDK classes.
  * Based off the official PSR-4 autoloader example found here:
  * https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
  *
@@ -43,10 +21,10 @@ if (version_compare(PHP_VERSION, '5.4.0', '<')) {
 spl_autoload_register(function ($class)
 {
   // project-specific namespace prefix
-  $prefix = 'Facebook\\';
+  $prefix = 'Tecnospeed\\';
 
   // base directory for the namespace prefix
-  $base_dir = defined('FACEBOOK_SDK_V4_SRC_DIR') ? FACEBOOK_SDK_V4_SRC_DIR : __DIR__ . '/src/Facebook/';
+  $base_dir = defined('TECNOSPEED_SDK_V1_SRC_DIR') ? TECNOSPEED_SDK_V4_SRC_DIR : __DIR__ . '/src/Tecnospeed/';
 
   // does the class use the namespace prefix?
   $len = strlen($prefix);
