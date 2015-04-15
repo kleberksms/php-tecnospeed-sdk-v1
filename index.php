e2cab5cfd38cbcb318f798c9e4e4b4238caac78c
Tiago Cristiano Alves <sistemas.tiago@bellinatiperez.com.br>
2015-04-15 16:09:41 -0300

    Criado arquivo com exemplos de tx2 separado por cidades

--- index.php
+++ index.php
@@ -1,97 +1,19 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
 $time_start = microtime(true);
-$rps = 1;
-$i   = 1;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
 $nf = new \Tecnospeed\NF();
 $arrayResp = array();
+$arrayTx2 = require_once  __DIR__ .'/src/Tecnospeed/Tx2Teste/Tx2.php';
+
+
+$rps = 1;
+$i   = 1;
 
 while($i > 0) {
 
-    $data = array(
-        'formato'=>'tx2',
-        'padrao'=>'TecnoNFSe',
-        'nomeCidade'=>'JOAOPESSOA',
-        'IdLote'=>'L1',
-        'numeroLote'=>'1',
-        'versao'=>'1',
-        'quantidadeRPS'=>'1',
-        'transacao'=>true,
-        'metodoEnvio'=>'WS',
-        'cpfCnpjRemetente'=>'03404018003081',
-        'inscricaoMunicipalRemetente'=>'1050052',
-        'razaoSocialRemetente'=>'Advocacia Bellinati Perez',
-        'codigoCidadeRemetente'=>'4115200',
-        'dataInicio'=>'2015-04-14T00:00:00',
-        'dataFim'=>'2015-04-14T00:00:00',
-        'valorTotalServicos'=>'46.82',
-        'valorTotalDeducoes'=>'0',
-        'valorTotalBaseCalculo'=>'1.00',
-        'idIntegracao'=>367495,
-        'idRps'=>'R1',
-        'situacaoNota'=>'1',
-        'tipoRps'=>'1',
-        'serieRps'=>'U',
-        'numeroRps'=>$rps,
-        'dataEmissao'=>'2015-04-14T00:00:00',
-        'competencia'=>'2015-04-09',
-        'cpfCnpjPrestador'=>'03404018003081',
-        'inscricaoMunicipalPrestador'=>'1050052git st',
-        'razaoSocialPrestador'=>'Advocacia Bellinati Perez',
-        'codigoCidadePrestacao'=>'4115200',
-        'descricaoCidadePrestacao'=>'Joinville',
-        'optanteSimplesNacional'=>'2',
-        'incentivadorCultural'=>'2',
-        'regimeEspecialTributacao'=>'0',
-        'naturezaTributacao'=>'1',
-        'incentivoFiscal'=>'2',
-        'cpfCnpjTomador'=>'07207996000150',
-        'razaoSocialTomador'=>'BANCO BRADESCO FINANCIAMENTOS S.A.',
-        'inscricaoEstadualTomador'=>'9044016688',
-        'tipoLogradouroTomador'=>'AVENIDA',
-        'enderecoTomador'=>'NUCLEO CIDADE DE DEUS S/N, PREDIO CINZA 1º ANDAR - VILA YARA',
-        'numeroTomador'=>'99',
-        'complementoTomador'=>'',
-        'tipoBairroTomador'=>'ZONA',
-        'bairroTomador'=>'NAO IDENTIFICADO',
-        'codigoCidadeTomador'=>'4115200',
-        'descricaoCidadeTomador'=>'MARINGA',
-        'ufTomador'=>'PR',
-        'cepTomador'=>'87020025',
-        'paisTomador'=>'1058',
-        'dddTomador'=>'44',
-        'telefoneTomador'=>'30379500',
-        'emailTomador'=>'sistemas.tiago@bellinati.com.br',
-        'codigoItemListaServico'=>'1714',
-        'codigoTributacaoMunicipio'=>'',
-        'codigoCnae'=>'6611801',
-        'discriminacaoServico'=>'HONORARIOS ADVOCATICIOS',
-        'tipoTributacao'=>7,
-        'exigibilidadeISS'=>'1',
-        'operacao'=>'',
-        'municipioIncidencia'=>'2507507',
-        'valorServicos'=>'46.82',
-        'aliquotaPIS'=>'0',
-        'aliquotaCOFINS'=>'0.00',
-        'aliquotaINSS'=>'0.00',
-        'aliquotaIR'=>'0.00',
-        'aliquotaCSLL'=>'0.00',
-        'valorPIS'=>'0.00',
-        'valorCOFINS'=>'0.00',
-        'valorINSS'=>'0.00',
-        'valorIR'=>'0.00',
-        'valorCSLL'=>'0.00',
-        'outrasRetencoes'=>'0',
-        'descontoIncondicionado'=>'0',
-        'descontoCondicionado'=>'0.00',
-        'valorDeducoes'=>'0.00',
-        'baseCalculo'=>'0',
-        'aliquotaISS'=>'0',
-        'valorIss'=>'0',
-        'valorISSRetido'=>'0.00',
-        'issRetido'=>'2',
-        'valorLiquidoNfse'=>'46.82',
-    );
+    $data = $arrayTx2['SaoPaulo'];
 
     try {
         $result = $nf->sendWithSocket($data);
