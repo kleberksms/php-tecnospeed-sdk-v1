894c5f9f72c8b128786b491a1521d3ec3a8ea259
Tiago Cristiano Alves <sistemas.tiago@bellinatiperez.com.br>
2015-05-06 17:55:20 -0300

    Parametrizado todas as ficilias no arquivo de configurao das Cities

--- src/Tecnospeed/Config/Cities.php
+++ src/Tecnospeed/Config/Cities.php
@@ -12,15 +12,15 @@ return array(
         'grupo'=>'Maringa',
         'versao'=>'2.01',
         'ExcludeAfterSend'=>true
-
     ),
+
     '03404018003081'=>array( // JoaoPessoa
         'CNPJ'=>'03404018003081',
         'grupo'=>'JoaoPessoa',
         'versao'=>'2.01',
         'ExcludeAfterSend'=>false
-
     ),
+
     '03404018002603'=>array( //SaoPaulo
         'CNPJ'=>'03404018002603',
         'grupo'=>'SaoPaulo',
@@ -34,5 +34,108 @@ return array(
             'versao'=>'1',
             'ExcludeAfterSend'=>false,
      ),
+
+    '03404018003677' => array( // CuritibaBatel
+        'CNPJ'=>'03404018000813',
+        'grupo'=>'CuritibaBatel',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+    ),
+
+    '03404018001542' => array( //CuritibaMarechal
+        'CNPJ'=>'03404018001542',
+        'grupo'=>'CuritibaMarechal',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+     ),
+
+    '03404018004568' => array( //CtbaCebp
+        'CNPJ'=>'03404018004568',
+        'grupo'=>'CtbaCebp',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+    ),
+
+    '03404018002000' => array( //PortoAlegre
+        'CNPJ'=>'03404018002000',
+        'grupo'=>'PortoAlegre',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+    ),
+
+    '03404018001208' => array( //PassoFundo
+        'CNPJ'=>'03404018001208',
+        'grupo'=>'PassoFundo',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+    ),
+
+    '03404018004304' => array( //PresidentePrudente
+        'CNPJ'=>'03404018004304',
+        'grupo'=>'PresidentePrudente',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+    ),
+
+    '03404018001623' => array( //BeloHorizonte
+        'CNPJ'=>'03404018001623',
+        'grupo'=>'BeloHorizonte',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+    ),
+
+    '03404018000902' => array( //Divinopolis
+        'CNPJ'=>'03404018000902',
+        'grupo'=>'Divinopolis',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+    ),
+
+    '03404018002271' => array( //CampoGrande
+        'CNPJ'=>'03404018002271',
+        'grupo'=>'CampoGrande',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+    ),
+
+    '03404018002514' => array( //Belem
+        'CNPJ'=>'03404018002514',
+        'grupo'=>'Belem',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+   ),
+
+    '03404018001461' => array(//Maceio
+        'CNPJ'=>'03404018001461',
+        'grupo'=>'Maceio',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+    ),
+
+    '03404018001976' => array(//Recife
+        'CNPJ'=>'03404018001976',
+        'grupo'=>'Recife',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+    ),
+
+    '03404018002867' => array(//Fortaleza
+        'CNPJ'=>'03404018002867',
+        'grupo'=>'Fortaleza',
+        'versao'=>'1',
+        'ExcludeAfterSend'=>false,
+    )
+
+
+
+
+
+
+
+
+
+
+
+
 
 );
