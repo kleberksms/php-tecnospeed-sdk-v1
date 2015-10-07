90eff482663517da358f63786bfffb8b677ab611
Fabiano Cruz <sistemas.fabiano@bellinatiperez.com.br>
2015-10-07 18:38:00 -0300

    Method Cancela

--- src/Tecnospeed/HttpClient/TecnospeedApi.php
+++ src/Tecnospeed/HttpClient/TecnospeedApi.php
@@ -205,14 +205,17 @@ class TecnospeedApi {
      * @return mixed
      * @link http://ciranda.me/tsdn/base-de-conhecimento/post/manual-manager-edoc-nfse-http-imprime-pdf
      */
-    public function pdf($cnpj, $idIntegracao)
+    public function pdf($cnpj, $idIntegracao, $nnfse = false)
     {
         if(is_null($idIntegracao)) {
             throw new \InvalidArgumentException('Informe o codigo de integracao para a pesquisa');
         }
 
-       $this->cnpjFilial = $cnpj;
-       $nnfse   = $this->getNumNFse($cnpj, $idIntegracao);
+        $this->cnpjFilial = $cnpj;
+
+        if(empty($nnfse)) {
+            $nnfse   = $this->getNumNFse($cnpj, $idIntegracao);
+        }
 
         $parameters = array(
             'CNPJ'       => $this->cnpjFilial,
@@ -225,8 +228,8 @@ class TecnospeedApi {
         $this->method    = 'imprime';
 
         $result = $this->generateUrl($parameters)
-                       ->curlConfig()
-                       ->getData();
+            ->curlConfig()
+            ->getData();
         return $result;
 
     }
@@ -365,7 +368,6 @@ class TecnospeedApi {
         return $result;
     }
 
-
     /**
      * Metodo para Cancelar notas em estado AUTORIZADA no Manager E-doc
      * @param $data array
@@ -373,21 +375,17 @@ class TecnospeedApi {
      * @throws \Exception
      * @link http://ciranda.me/tsdn/base-de-conhecimento/post/manual-manager-edoc-nfse-http-cancela
      * $data = array(
-            'cnpj'      => '03404018002867',
-            'idManager' => '72236',
-            'nrps'      => '3451',
-        );
+    'cnpj'      => '03404018002867',
+    'idManager' => '72236',
+    'nrps'      => '3451',
+    );
      * @exemple 1 $result = $api->cancela($data));
      * @exemple 2 $result = $api->cancela(array('cnpj'=> '03404018002867','idManager'=>72236,'nrps'=>3451));
      */
     public function cancela($data)
     {
-        if(!is_array($data)) {
+        if(!is_array($data) || !isset($data['cnpj']) || !isset($data['idManager']) || !isset($data['nrps']) || !isset($data['codNF'])) {
             throw new \InvalidArgumentException ('Dados não informados!');
-        }
-
-        if(!isset($data['cnpj'])) {
-            throw new \InvalidArgumentException ('Informe o Cnpj da Filial!');
         }
 
         $this->cnpjFilial   = Filter::returnOnlyNumbers($data['cnpj']);
@@ -399,6 +397,7 @@ class TecnospeedApi {
             'grupo'         => $this->cities[$this->cnpjFilial]['grupo'],
             'NomeCidade'    => $this->cities[$this->cnpjFilial]['nomeCidade'],
             'Handle'        => $data['idManager'],
+            'idIntegracao'  => $data['codNF'],
             'nrps'          => $data['nrps'],
         );
 
